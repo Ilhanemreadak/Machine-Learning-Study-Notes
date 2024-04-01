845b0867b0958ede050513f4c4743b293473fb8b
İlhan Emre ADAK <92309151+Ilhanemreadak@users.noreply.github.com>
2024-04-01 18:33:09 +0300

    Add files via upload

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -1,26 +1,45 @@
 import pandas as pd
 
-dataset = pd.read_csv("Veriler/breast-cancer.csv")
-print(dataset.head())
+dataset = pd.read_csv("Veriler/breast-cancer.csv") # Verilerimizi çekiyoruz.
+print(dataset.head()) # Verilerimizi incelemek için verisetimizin ilk 5 elamanını yazıdırıyor.
 
 from sklearn.preprocessing import LabelEncoder
-labelencoder = LabelEncoder()
+
+# Hatırlatma : LabelEncoder, kategorik (sınıflandırılabilir) verileri sayısal değerlere dönüştürmek için kullanılan bir araçtır.
+
+labelencoder = LabelEncoder() # LabelEncoder nesnemizi oluşturuyoruz.
+
+# LabelEncoder ile "diagnosis" yani teşhislerimizi sayısal değerlere çeviriyoruz. Örn: Kanser = 1 , Kanser Değil = 0
 dataset["diagnosis"] = labelencoder.fit_transform(dataset["diagnosis"].values)
 
-X = dataset.drop("diagnosis", axis=1)
-y = dataset["diagnosis"]
+X = dataset.drop("diagnosis", axis=1) # Eğitim setimizden teşhisleri çıkarıyoruz çünkü hedefimiz teşhis etmek.
+y = dataset["diagnosis"] # Teşhis başlığını ve altındaki değerleri yani tüm sütunu çıkış testimize atıyoruz.
 
 from sklearn.model_selection import train_test_split
 
+"""
+
+Bu satırda, veri seti X ve hedef değişken y arasındaki ilişkiyi koruyarak, 
+X ve y verilerini eğitim ve test setlerine ayırmak için train_test_split fonksiyonu kullanılarak X_train, X_test, y_train, y_test değişkenlerine atama yapılmıştır.
+Eğitim verisi, toplam veri setinin %30'u olarak belirlenmiştir.
+"""
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.3)
 
 from sklearn.naive_bayes import GaussianNB
 
-model = GaussianNB()
-model.fit(X_train, y_train)
+"""
+Bu satırda, veri seti X ve hedef değişken y arasındaki ilişkiyi koruyarak, 
+X ve y verilerini eğitim ve test setlerine ayırmak için train_test_split fonksiyonu kullanılarak X_train, X_test, y_train, y_test değişkenlerine atama yapılmıştır. 
+Eğitim verisi, toplam veri setinin %30'u olarak belirlenmiştir.
+"""
 
-predictions = model.predict(X_test)
-print(predictions)
+model = GaussianNB() # GaussianNB Modelimizi oluşturuyoruz.
+model.fit(X_train, y_train) # Verimizi modelimize oturtuyoruz.
+
+predictions = model.predict(X_test)# Modelimiz artık tahminlerini yapıyor ve sonuçları atıyoruz.
+print(predictions) # Sonuçlarımızı yazdırıyoruz.
+
+# GaussianNB modelimizin performans ölçümlerimizi yapıyoruz.
 
 from sklearn.metrics import confusion_matrix
 
